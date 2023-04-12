eaf11291fc7262d4f825c64bf04366c069b80fa2
iamjoee <joeadvocates123@gmail.com>
2023-04-12 14:14:23 -0700

    input/output task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""this defines the class Student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """this represents the student"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
         Args:
-name_first (str): 1st name of student
-name_last (str): 2nd name of student
-age (int): age of student
-"""
-self.first_name = name_first
-elf.last_name = last_name
-self.age = age
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self , attrs=None);
-"""this gets the  dictionary representation of Student
-
-If attrs is a list of strings, represents only those attributes
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
         included in the list.
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-if (type(attrs) == list and
+        if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-
