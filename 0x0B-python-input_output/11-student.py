38e8c792e0a5b91d53c31de69c690e9b57c6947c
iamjoee <joeadvocates123@gmail.com>
2023-04-12 14:31:00 -0700

    input/output task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Contains class student
+this contains class"Student"
 """
 
 
 class Student:
-    """Represents the  student"""
+    """this represents students"""
     def __init__(self, first_name, last_name, age):
-        """Initializes  student"""
+        """this nitializes student"""
         self.first_name = first_name
-        self.second_name = second_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns the dictionary representation of the Student instance
+        """this returns dictionary representation of student instance
         with specified attributes"""
         if attrs is None:
             return self.__dict__
@@ -26,7 +26,7 @@ class Student:
         return new_dict
 
     def reload_from_json(self, json):
-        """this replaces all attributes of Student instance"""
+        """replaces  attributes of Student instance"""
         for key in json:
             try:
                 setattr(self, key, json[key])
