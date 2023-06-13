bb037ab22f992cd054ec17188f5b2898d9fd2e5d
iamjoee <joeadvocates123@gmail.com>
2023-06-13 11:11:12 -0700

    javascript update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -16,15 +16,6 @@ class Rectangle {
       console.log(s);
     }
   }
-
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
 }
+
+module.exports = Rectangle;
