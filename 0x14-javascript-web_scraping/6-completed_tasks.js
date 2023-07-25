97af0acc7b1d3df925590e63b5723f97707881ec
iamjoee <joeadvocates123@gmail.com>
2023-07-25 01:02:46 -0700

    Web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+
+  const completedTasks = {};
+
+  tasks.forEach((task) => {
+    if (task.completed) {
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId]++;
+      } else {
+        completedTasks[task.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+});
+
